90e39b19ffa744d043e823a0b3a971fbd551c7d3
oferwshop <46059654+oferwshop@users.noreply.github.com>
2018-12-22 02:39:31 +0200

    Update App.js

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -14,14 +14,6 @@ class App extends Component {
           <h1>
             { this.state.msg }
           </h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
