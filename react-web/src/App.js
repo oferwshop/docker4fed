2f3551c0557894ece6f59648b00b637fc6f7d2da
oferwshop <46059654+oferwshop@users.noreply.github.com>
2018-12-22 11:17:12 +0200

    Update App.js

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -10,8 +10,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>
+        <div style="height: 277px;max-height: 300px;">
+            <img src={logo} class="App-logo" alt="logo" style="height: 90%;">
+            <img src="https://dwglogo.com/wp-content/uploads/2017/09/1300px-Docker_container_engine_logo.png" style="height: 100%;padding-top: 18px;" class="App-logo">
+        </div>
+          <h1 style="z-index: 9999;margin-top: 100px;">
             { this.state.msg }
           </h1>
         </header>
